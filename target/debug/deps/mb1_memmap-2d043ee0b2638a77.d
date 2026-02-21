dcdb29a2435c85a75ff13186222691368c8d1f3a
Lucas Simpson <lucas.simpson0213@gmail.com>
2026-02-21 01:18:51 -0600

    fix: add .gitignore

--- target/debug/deps/mb1_memmap-2d043ee0b2638a77.d
+++ target/debug/deps/mb1_memmap-2d043ee0b2638a77.d
@@ -1,8 +1,8 @@
-/Users/lucassimpson/code/rust/memmap/target/debug/deps/mb1_memmap-2d043ee0b2638a77.d: src/lib.rs src/frames.rs src/raw.rs src/region.rs src/tests.rs src/tests/common.rs src/tests/prelude.rs
+/Users/lucassimpson/code/rust/userland-kernel-rust/target/debug/deps/mb1_memmap-2d043ee0b2638a77.d: src/lib.rs src/frames.rs src/raw.rs src/region.rs src/tests.rs src/tests/common.rs src/tests/prelude.rs
 
-/Users/lucassimpson/code/rust/memmap/target/debug/deps/libmb1_memmap-2d043ee0b2638a77.rlib: src/lib.rs src/frames.rs src/raw.rs src/region.rs src/tests.rs src/tests/common.rs src/tests/prelude.rs
+/Users/lucassimpson/code/rust/userland-kernel-rust/target/debug/deps/libmb1_memmap-2d043ee0b2638a77.rlib: src/lib.rs src/frames.rs src/raw.rs src/region.rs src/tests.rs src/tests/common.rs src/tests/prelude.rs
 
-/Users/lucassimpson/code/rust/memmap/target/debug/deps/libmb1_memmap-2d043ee0b2638a77.rmeta: src/lib.rs src/frames.rs src/raw.rs src/region.rs src/tests.rs src/tests/common.rs src/tests/prelude.rs
+/Users/lucassimpson/code/rust/userland-kernel-rust/target/debug/deps/libmb1_memmap-2d043ee0b2638a77.rmeta: src/lib.rs src/frames.rs src/raw.rs src/region.rs src/tests.rs src/tests/common.rs src/tests/prelude.rs
 
 src/lib.rs:
 src/frames.rs:
--- target/debug/deps/mb1_memmap-e5f7899e7a841640.d
+++ target/debug/deps/mb1_memmap-e5f7899e7a841640.d
@@ -1,6 +1,6 @@
-/Users/lucassimpson/code/rust/memmap/target/debug/deps/mb1_memmap-e5f7899e7a841640.d: src/lib.rs src/frames.rs src/raw.rs src/region.rs src/tests.rs src/tests/common.rs src/tests/prelude.rs
+/Users/lucassimpson/code/rust/userland-kernel-rust/target/debug/deps/mb1_memmap-e5f7899e7a841640.d: src/lib.rs src/frames.rs src/raw.rs src/region.rs src/tests.rs src/tests/common.rs src/tests/prelude.rs
 
-/Users/lucassimpson/code/rust/memmap/target/debug/deps/mb1_memmap-e5f7899e7a841640: src/lib.rs src/frames.rs src/raw.rs src/region.rs src/tests.rs src/tests/common.rs src/tests/prelude.rs
+/Users/lucassimpson/code/rust/userland-kernel-rust/target/debug/deps/mb1_memmap-e5f7899e7a841640: src/lib.rs src/frames.rs src/raw.rs src/region.rs src/tests.rs src/tests/common.rs src/tests/prelude.rs
 
 src/lib.rs:
 src/frames.rs:
